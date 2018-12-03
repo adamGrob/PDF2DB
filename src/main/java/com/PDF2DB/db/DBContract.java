44c9a539791177f3caac8ccc70347f41b71ea955
adamGrob <ad4mgr0b@gmail.com>
2018-12-03 19:47:42 +0100

    PSQL database contract file, fill this with your DV credentials.

---
+++ src/main/java/com/PDF2DB/db/DBContract.java
@@ -0,0 +1,8 @@
+package com.PDF2DB.db;
+
+public interface DBContract {
+    public static final  String HOST = "yourDbHostName";
+    public static final  String DB_NAME = "yourDbName";
+    public static final  String USERNAME = "YourUseName";
+    public static final  String PASSWORD = "YourDBPass";
+}
