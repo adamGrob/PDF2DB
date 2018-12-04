146f2731713d72f9b922ba20286ee6b79ac2edfb
adamGrob <ad4mgr0b@gmail.com>
2018-12-04 16:48:20 +0100

    Removed the try catch block, added the throws to the method signature instead.

--- src/main/java/com/PDF2DB/Main.java
+++ src/main/java/com/PDF2DB/Main.java
@@ -1,6 +1,7 @@
 package com.PDF2DB;
 
-import com.PDF2DB.parser.Parser;
+import com.PDF2DB.controller.SegmentCreator;
+import com.PDF2DB.controller.parser.Parser;
 import org.apache.tika.exception.TikaException;
 import org.xml.sax.SAXException;
 
@@ -8,16 +9,13 @@ import java.io.IOException;
 
 public class Main {
 
-    public static void main(final String[] args) {
+    public static void main(final String[] args) throws TikaException, IOException, SAXException {
 
         String pdfToParse = "/home/grobadam/IdeaProjects/PDF2DB/src/main/resources/RollCall.pdf";
-
         Parser parser = new Parser(pdfToParse);
-        try {
-            parser.parse();
-        } catch (IOException | TikaException | SAXException e) {
-            e.printStackTrace();
-        }
+        SegmentCreator segmentCreator = new SegmentCreator(parser.getStringFromPdf());
+        segmentCreator.createVoteSegments();
+        segmentCreator.printSegments();
 
 
     }
