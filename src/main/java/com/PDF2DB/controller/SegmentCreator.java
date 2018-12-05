e7e7cbbed69181d6fb2a9014f987907799b6514e
adamGrob <ad4mgr0b@gmail.com>
2018-12-05 20:53:08 +0100

    Added imports, for necessary Map and List interfaces. Added new Patterns. The hashmap now stores List<List<String>> instead of just List<String> as values. Extended the getVoteSegmentsOFACase function for correcty filling up the hashmap of the voters. Added a cleanVoterList function for clearing up the resultLists from unnecessary characters, like white spaces and line breaks.

--- src/main/java/com/PDF2DB/controller/SegmentCreator.java
+++ src/main/java/com/PDF2DB/controller/SegmentCreator.java
@@ -1,9 +1,6 @@
 package com.PDF2DB.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,7 +8,7 @@ public class SegmentCreator {
 
     private String stringCreateSegmentsFrom;
 
-    private Map<String, List<String>> rollCallMap = new HashMap<>();
+    private Map<String, List<List<String>>> rollCallMap = new HashMap<>();
 
     public SegmentCreator(String stringCreateSegmentsFrom) {
         this.stringCreateSegmentsFrom = stringCreateSegmentsFrom;
@@ -21,7 +18,8 @@ public class SegmentCreator {
         // (?s)(EN\s)(.{1,300})(\s\n\n\d+\.\d+\.\sFinal\svote\s\n\n)(\d+\s[\+\-0]\s\n{2})(.*?)(Corrections to vote)  this is the greater pattern
         // this pattern can cut down all the voting segments (?s)(\d+\s[\+\-0]\s\n{2})(.*?)(\s\n\n\s\n\n)
         // pattern to extract the case the vote happens on from the votesegments: (?s)(?<=(EN)\s\n\n(\d)\.\s)(.*?)(?=\d+\.\d+\.\sFinal vote)
-
+        // pattern for separating party voters from each other(works well except for the last row/party)   (?s)(([A-Z]+([&\\\/])*[A-Z]+))(.*?)(?=([A-Z]+([&\\\/]*)[A-Z]+))
+        // code for removing all the party names: voteSegment.replaceAll("(([A-Z]+([&\\\\\\/])*[A-Z]+))", "")
         String patternString = "(?s)(EN\\s)(.{1,300})(\\s\\n\\n\\d+\\.\\d+\\.\\sFinal\\svote\\s\\n\\n)(\\d+\\s[\\+\\-0]\\s\\n{2})(.*?)(Corrections to vote)";
         Pattern pattern = Pattern.compile(patternString);
         Matcher matcher = pattern.matcher(stringCreateSegmentsFrom);
@@ -29,19 +27,19 @@ public class SegmentCreator {
         while(matcher.find()) {
             String voteSegment = stringCreateSegmentsFrom.substring(matcher.start(), matcher.end());
             String nameOfTheCaseToVoteOn = getTheNameOfTHeCaseToVoteOn(voteSegment, "(?s)(?<=(EN)\\s\\n\\n(\\d)\\.\\s)(.*?)(?=\\d+\\.\\d+\\.\\sFinal vote)");
-            List<String> voteSegmentsOFACase = getVoteSegmentsOFACase(voteSegment, "(?s)(\\d+\\s[\\+\\-0]\\s\\n{2})(.*?)(\\s\\n\\n\\s\\n\\n)");
+            List<List<String>> voteSegmentsOFACase = getVoteSegmentsOFACase(voteSegment, "(?s)(\\d+\\s[\\+\\-0]\\s\\n{2})(.*?)(\\s\\n\\n\\s\\n\\n)");
             rollCallMap.put(nameOfTheCaseToVoteOn, voteSegmentsOFACase);
         }
     }
 
     public void printSegments () {
-        for (Map.Entry<String, List<String>> entry : rollCallMap.entrySet()) {
+        for (Map.Entry<String, List<List<String>>> entry : rollCallMap.entrySet()) {
             System.out.println("----------------------------------------------");
             String key = entry.getKey();
-            List<String> value = entry.getValue();
+            List<List<String>> value = entry.getValue();
             System.out.println(key);
-            for (String vote: value) {
-                System.out.println(vote);
+            for (List<String> voters: value) {
+                System.out.println(voters);
             }
             System.out.println("----------------------------------------------");
         }
@@ -57,14 +55,31 @@ public class SegmentCreator {
         return "Pattern not found";
     }
 
-    private List<String> getVoteSegmentsOFACase(String stringToCreateSegmentsFrom, String regex) {
-        List<String> voteSegmentList = new ArrayList<>();
+    private List<List<String>> getVoteSegmentsOFACase(String stringToCreateSegmentsFrom, String regex) {
+        List<List<String>> voteSegmentList = new ArrayList<>();
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(stringToCreateSegmentsFrom);
         while(matcher.find()) {
             String voteSegment = stringToCreateSegmentsFrom.substring(matcher.start(), matcher.end());
-            voteSegmentList.add(voteSegment);
+            voteSegment = voteSegment.replaceAll("(([A-Z]+([&\\\\\\/])*[A-Z]+))", ""); // remove the party names
+            //voteSegmentList.add(voteSegment);
+            String[] voterList = voteSegment.split(",|\\n\\n\\s");
+            List<String> cleanVoterList = cleanVoterList(voterList);
+            System.out.println(cleanVoterList);
+            voteSegmentList.add(cleanVoterList);
         }
         return voteSegmentList;
+    }
+
+    private List<String> cleanVoterList(String[] voterList) {
+        List<String> cleanedVoterList = new ArrayList<>();
+        for (String voter: voterList) {
+            if (!voter.equals("") && !voter.equals(" ")) {
+                String trimmedVoter = voter.trim();
+                trimmedVoter = trimmedVoter.replaceAll("\n\n", "");
+                cleanedVoterList.add(trimmedVoter);
+            }
+        }
+        return cleanedVoterList;
     }
 }
